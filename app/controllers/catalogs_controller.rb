24c43b1eab357c5e9009cb02f6f90cb6304fdaed
deror1869107 <sungyuanyao@gmail.com>
2017-04-29 13:34:15 +0800

    Add Catalog create

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -2,4 +2,15 @@ class CatalogsController < ApplicationController
   def all
     render :json => Catalog.all
   end
+
+  def create
+    params.permit!
+    Catalog.create(catalog_params)
+  end
+
+  private
+
+  def catalog_params
+    params.require(:catalog)
+  end
 end
